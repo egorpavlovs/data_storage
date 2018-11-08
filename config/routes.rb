b557eafe23ef5f2059fd167746ed0eaf95ef069f
Egor Pavlov <pavlov_egor007@mail.ru>
2018-11-09 01:04:14 +0300

    Update routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: "rails_admin/main#dashboard"
 
-  scope '/', controller: 'base_api_controller' do
+  scope '/api', controller: 'base_api_controller' do
     post '/', action: 'show'
   end
 end
