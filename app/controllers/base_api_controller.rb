c08c3cad0226c5dcafb84254844c6014ee65ab6c
Egor Pavlov <pavlov_egor007@mail.ru>
2018-11-09 03:46:54 +0300

    Update api controller

--- app/controllers/base_api_controller.rb
+++ app/controllers/base_api_controller.rb
@@ -1,6 +1,6 @@
 class BaseApiController < ApplicationController
   protect_from_forgery with: :null_session
-  before_action :parse_request, :find_news_item
+  before_action :find_news_item
 
   def show
     render json: @news_items
@@ -8,16 +8,14 @@ class BaseApiController < ApplicationController
 
   private
 
-    def parse_request
-      pp ["params", params]
-      pp ["params[search_field]", params["search_field"]]
-      pp ["JSON.parse(params[search_field])", JSON.parse(params["search_field"])]
-      pp ["JSON.parse(params)", JSON.parse(params)]
-      @json = params["search_field"]
-    end
+    # def parse_request
+    #   pp ["params", params]
+    #   pp ["params[search_field]", params["search_field"]]
+    #   @json = params["search_field"]
+    # end
 
     def find_news_item
-      @news_items = NewsItem.where("news_items.title ilike ?", "%#{@json["search_field"]}%")
+      @news_items = NewsItem.where("news_items.title ilike ?", "%#{params["search_field"]}%")
     end
 
 end
