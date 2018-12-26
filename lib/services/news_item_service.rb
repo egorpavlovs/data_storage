3965086608ede813d6bd445e410bc26ff5d9751d
Egor Pavlov <pavlov_egor007@mail.ru>
2018-12-26 16:08:51 +0300

    Update search

--- lib/services/news_item_service.rb
+++ lib/services/news_item_service.rb
@@ -1,8 +1,13 @@
 class NewsItemService < ApplicationService
   class << self
     def search(search_field)
-      words = search_field.split(/\W+/).map {|val| "%#{val}%" }
-      NewsItem.where("title ilike any (array[:search]) or description ilike any (array[:search])", search: words)
+      pp ['search_field', search_field]
+      line_search = NewsItem.where("news_items.title ilike ?", "%#{search_field}%")
+      unless line_search.present?
+        words = search_field.split(/\W+/).map {|val| "%#{val}%" }
+        line_search = NewsItem.where("title ilike any (array[:search]) or description ilike any (array[:search])", search: words)
+      end
+      line_search
     end
   end
 end
